de2eb3f06782281d9cbc28287695b67b7da2084a
Ömer Esmer <iskipperlla.437@gmail.com>
2021-06-26 06:38:01 +0300

    Add Firebase 2

--- firebase/client.js
+++ firebase/client.js
@@ -8,7 +8,7 @@ const credentials = {
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGİNG_SENDER_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
