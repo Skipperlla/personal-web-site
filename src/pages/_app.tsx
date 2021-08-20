5608c92787108091a56df366a5354ff3a6f44e81
Ömer Esmer <iskipperlla.437@gmail.com>
2021-08-21 00:15:34 +0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,37 +1,15 @@
 import "@assets/styles/globals.css";
 import Layout from "@layout/Layout";
 import ExportIcons from "../../scripts/Icons";
-import { AppProps, NextWebVitalsMetric } from "next/app";
-import { useEffect, FC } from "react";
-import { useRouter } from "next/router";
-import * as gtag from "@lib/gtag";
+import { AppProps } from "next/app";
 
-const Noop: FC = ({ children }) => <>{children}</>;
 function MyApp({ Component, pageProps }: AppProps) {
   ExportIcons();
-  const LayoutNoop = (Component as any).LayoutNoop || Noop;
-  // remove chrome-bug.css loading class on FCP
-  useEffect(() => {
-    document.body.classList?.remove("loading");
-  }, []);
 
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
   return (
-    <LayoutNoop pageProps={pageProps}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </LayoutNoop>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
